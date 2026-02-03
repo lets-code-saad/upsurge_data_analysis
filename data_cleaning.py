8c38b9fcffe882d3f5c416bd5c115dac959fa7a7
Saad <saadabbas594@gmail.com>
2026-02-03 16:52:01 +0500

    imported csv fiels

---
+++ data_cleaning.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+# Data Importing
+
+customers_table = pd.read_csv("/e_commerce_data/customers.csv")
+orders_table = pd.read_csv("/e_commerce_data/orders.csv")
+products_table = pd.read_csv("/")
+
+# Cleaning
+
+# Handle missing values (justify your choices)
+
+# Fix data types
+
+# Remove invalid rows (if any)
+
+# Analysis
+
+# Recalculate monthly revenue
+
+# Identify repeat customers
+
+# Find top category per month
+
+# Detect outliers in order quantity
